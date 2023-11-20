bd723f90907f5f493524139f0a8e105578a13a59
Sidique <sidiqueakim@gmail.com>
2023-11-20 16:54:45 +0000

    added the event

---
+++ client/src/Components/EventRouteSelector/index.js
@@ -0,0 +1,198 @@
+import React, { useState, useEffect } from "react"
+import { View, Text, SafeAreaView, ScrollView, StyleSheet } from "react-native"
+
+import AppButton from "../AppButton"
+import AppTextInput from "../AppTextInput"
+import GetAsync from "../AsyncStorageGet"
+import RouteParamsModal from "../RouteParamsModal"
+import SlideBox from "../SlideBox"
+import moment from "moment"
+import validator from "validator"
+
+// i need a form that takes start pastcode, jounrey pref, mode, walking speed, taxi only, national search
+
+export default function EventsRoutesForm({ event }) {
+	const formatDate = "YYYYMMDD"
+	const formatTime = "HHmm"
+	const [endPostcodeInput, setEndPostcodeInput] = useState(event.postcode, "GB")
+	const [startPostcodeInput, setStartPostcodeInput] = useState("")
+	const [token, setToken] = useState("")
+	const [route, setRoute] = useState(false)
+	const [paramsModal, setParamsModal] = useState(false)
+	const originalDate = moment(event.date)
+	// Initialize state with formatted date and time
+	const [fDate, setFDate] = useState(originalDate.format(formatDate))
+	const [fTime, setFTime] = useState(originalDate.format(formatTime))
+	const [selectedParams, setSelectedParams] = useState({
+		taxiOnlyChecked: false,
+		nationalSearch: false,
+		journeyPref: "",
+		mode: "",
+		walkingSpeed: "",
+	})
+	const handleParamsSelection = (params) => {
+		setSelectedParams(params)
+	}
+
+	// Parse the original date string
+
+	async function clickPreferences() {
+		setParamsModal(!paramsModal)
+	}
+	useEffect(() => {
+		async function getToken() {
+			setToken(await GetAsync("token"))
+		}
+		getToken()
+	}, [])
+	function dataValidation() {
+		if (!startPostcodeInput) {
+			alert("Fill in all fields.")
+		} else if (!validator.isPostalCode(startPostcodeInput.trim(), "GB")) {
+			alert("Enter a valid starting postcode.")
+		} else {
+			getRoute()
+		}
+	}
+	async function getRoute() {
+		const startPostcode = startPostcodeInput.trim()
+		const endPostcode = endPostcodeInput
+		const useDate = fDate || ""
+		const useTime = fTime || ""
+		const timeIs = "arriving"
+
+		console.log(startPostcode)
+		console.log(endPostcode)
+		console.log(useDate)
+		console.log(useTime)
+		console.log(timeIs)
+		console.log(selectedParams.mode)
+		console.log(selectedParams.walkingSpeed)
+		console.log(selectedParams.taxiOnlyChecked)
+		console.log(selectedParams.nationalSearch)
+		const options = {
+			method: "POST",
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json",
+				"Authorization": token,
+			},
+			body: JSON.stringify({
+				origins: {
+					from: startPostcode,
+					to: endPostcode,
+				},
+				params: {
+					taxiOnlyTrip: selectedParams.taxiOnlyChecked,
+					nationalSearch: selectedParams.nationalSearch,
+					date: useDate || "",
+					time: useTime || "",
+					timeIs: timeIs || "",
+					mode: selectedParams.mode || "",
+					walkingSpeed: selectedParams.walkingSpeed || "",
+					useRealTimeArrivals: true,
+				},
+			}),
+		}
+		const response = await fetch(
+			"https://metro-mingle.onrender.com/tfl/get",
+			options
+		)
+		console.log(response)
+		if (response.status == 200) {
+			const data = await response.json()
+			console.log(data)
+			setRoute(data.journeys)
+		} else {
+			alert("Request failed.")
+		}
+	}
+
+	async function setJourney(route) {
+		try {
+			const id = event.id
+			const options = {
+				method: "PATCH",
+				headers: {
+					"Accept": "application/json",
+					"Content-Type": "application/json",
+					"Authorization": token,
+				},
+				body: JSON.stringify({
+					event_id: id,
+					journey: route,
+				}),
+			}
+			const response = await fetch(
+				"https://metro-mingle.onrender.com/event/setroute",
+				options
+			)
+			console.log(response)
+			if (response.status == 200) {
+				alert("Route set.")
+			} else {
+				alert("Request failed.")
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	return (
+		<ScrollView>
+			<View style={styles.screen}>
+				<Text style={styles.heading}>Set your journey</Text>
+				<AppTextInput
+					onChangeText={(text) => setStartPostcodeInput(text)}
+					placeholder="Start Postcode"
+				/>
+
+				<AppButton onPress={clickPreferences} title="Preferences" />
+				{paramsModal ? (
+					<RouteParamsModal
+						paramsModal={paramsModal}
+						closeModal={clickPreferences}
+						onParamsSelect={handleParamsSelection}
+					></RouteParamsModal>
+				) : null}
+				<AppButton onPress={dataValidation} title="Submit" />
+			</View>
+
+			<SafeAreaView style={styles.container}>
+				<View>
+					{route ? (
+						<SlideBox
+							slides={[
+								{
+									journey: route[0],
+									content: "Route 1",
+									onSelect: () => setJourney(route[0]),
+								},
+								{
+									journey: route[1],
+									content: "Route 2",
+									onSelect: () => setJourney(route[1]),
+								},
+								{
+									journey: route[2],
+									content: "Route 3",
+									onSelect: () => setJourney(route[2]),
+								},
+							]}
+						/>
+					) : null}
+				</View>
+			</SafeAreaView>
+		</ScrollView>
+	)
+}
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		alignItems: "center",
+	},
+	heading: {
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+})
--- client/src/Screens/RoutesScreen/index.js
+++ client/src/Screens/RoutesScreen/index.js
@@ -1,20 +1,17 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, SafeAreaView } from "react-native"
 import RoutesScreenForm from "../../Components/RoutesScreenForm"
 
-
-export default function Routes(){
-
-    return(
-        <View style={styles.view}>
-            <RoutesScreenForm />
-        </View>
-    )
-    
+export default function Routes() {
+	return (
+		<View style={styles.view}>
+			<RoutesScreenForm />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        justifyContent: "center"
-    }
+	view: {
+		flex: 1,
+		justifyContent: "center",
+	},
 })
